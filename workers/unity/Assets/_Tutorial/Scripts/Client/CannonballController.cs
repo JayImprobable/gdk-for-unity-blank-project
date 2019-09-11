06229be39af58af0cbba696ffce1e2d91ca4cc9b
Jay Santos <jaysantos@improbable.io>
2019-09-11 15:31:45 +0100

    Cannonball not working at all :(
    Shelving it for now

--- workers/unity/Assets/_Tutorial/Scripts/Client/CannonballController.cs
+++ workers/unity/Assets/_Tutorial/Scripts/Client/CannonballController.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.Core.Commands;
 using Improbable.Gdk.Subscriptions;
+using Tank;
 using UnityEngine;
 
 public class CannonballController : MonoBehaviour
 {
     [Require] private EntityId entityId;
     [Require] private WorldCommandSender worldCommandSender;
+    [Require] private HealthCommandSender healthCommandSender;
 
     [SerializeField] private float blastRadius = 10;
     [SerializeField] private LayerMask layerMask;
@@ -21,6 +23,12 @@ public class CannonballController : MonoBehaviour
         {
             foreach (var v in hitColliders)
             {
+                if (v.gameObject.CompareTag("Tank"))
+                {
+                    Debug.Log($"I am {gameObject.name}");
+                    HealthModifier request = new HealthModifier(GameConstants.CannonDamage);
+                    healthCommandSender.SendUpdateHealthCommand(v.gameObject.GetComponent<LinkedEntityComponent>().EntityId, request,HealthCommandResponseReceived);
+                }
             }
         }
         
@@ -30,5 +38,9 @@ public class CannonballController : MonoBehaviour
             WorldCommands.DeleteEntity.Request request = new WorldCommands.DeleteEntity.Request(entityId);
             worldCommandSender.SendDeleteEntityCommand(request);
         }
+    }
+
+    private void HealthCommandResponseReceived(Tank.Health.UpdateHealth.ReceivedResponse response)
+    {
     }
 }
